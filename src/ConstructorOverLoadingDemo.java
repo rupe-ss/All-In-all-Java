bdabe6cdff01d0b4b4eef9cc89e0a7093293d82e
rupesss-09 <rupesss09@gmail.com>
2022-03-29 11:31:38 -0400

    Overloading Constructor

---
+++ src/ConstructorOverLoadingDemo.java
@@ -0,0 +1,25 @@
+
+//Practicing and Examples overloading constructor
+class CalDemo {
+    int a;
+    int b;
+
+    CalDemo(int a){
+        this.a = a;
+        System.out.println(this.a);
+    }
+
+    CalDemo(int a, int b){
+        this.a = a;
+        this.b = b;
+        System.out.println(this.a);
+        System.out.println(this.b);
+    }
+}
+
+public class ConstructorOverLoadingDemo {
+    public static void main(String[] args) {
+        CalDemo calDemo = new CalDemo(2);
+        CalDemo calDemo1 = new CalDemo(2,4);
+    }
+}
