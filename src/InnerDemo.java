552469e4969c7233543c9742b533ac7f6e8ebcd7
rupesss-09 <rupesss09@gmail.com>
2022-03-30 21:04:51 -0400

    We can create class inside class, we just have to do extra step to create the inner class object.

---
+++ src/InnerDemo.java
@@ -0,0 +1,27 @@
+/*
+Notes:
+1. We can make a member class inside a class just like making member variable and member method.
+2. To make a object of inner class we need a reference of parent class.
+ */
+class Outer{
+    int a;
+    public void show(){
+        System.out.println("This is a outer class");
+    }
+
+    class Inner{
+        public void display(){
+            System.out.println("Displaying from inner class.");
+        }
+    }
+}
+
+public class InnerDemo {
+    public static void main(String[] args) {
+        //This is how we create a object for regular class.
+        Outer outer = new Outer();
+        //In order to create a Object of inner class we need a object of main class then only we can create a object of inner class.
+        Outer.Inner inner = outer.new Inner();
+        inner.display();
+    }
+}
