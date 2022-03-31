5712b0b10a1d20f3aacc0e1431140f35f6c61839
rupesss-09 <rupesss09@gmail.com>
2022-03-30 21:19:19 -0400

    Varargs is a new thing I learned in Java, there is same in JavaScript as well.

---
+++ src/VarargsDemo.java
@@ -0,0 +1,19 @@
+class Addition{
+    public int add(int ... i){ //When you use 3 dots its call Variable length Arguments, now the arguments comes in the form of array not in Integer
+        int sum = 0;
+        //Since, argument is coming into form of array we have to use for loop to add now.
+        for(int k: i){
+            sum += k;
+        }
+        return sum;
+    }
+}
+
+public class VarargsDemo {
+    public static void main(String[] args) {
+        //Lets create a method that will add numbers
+        Addition addition = new Addition();
+        System.out.println(addition.add(1,2,3,4));
+        System.out.println(addition.add(3,4));
+    }
+}
