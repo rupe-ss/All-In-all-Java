e4ca861c780ef220db99aeda7652e50ee77b6674
rupesss-09 <rupesss09@gmail.com>
2022-04-01 19:48:06 -0400

    Varargs little update.

--- src/VarargsDemo.java
+++ src/VarargsDemo.java
@@ -2,6 +2,7 @@ class Addition{
     public int add(int ... i){ //When you use 3 dots its call Variable length Arguments, now the arguments comes in the form of array not in Integer
         int sum = 0;
         //Since, argument is coming into form of array we have to use for loop to add now.
+        System.out.println(i); //This will print hash code bacause i is an array here.
         for(int k: i){
             sum += k;
         }
