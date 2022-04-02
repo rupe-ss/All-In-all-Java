9a4037a4150449c91cbd4d9fde0c19b153add12a
rupesss-09 <rupesss09@gmail.com>
2022-04-02 12:09:29 -0400

    Interface with anonymous class.

---
+++ src/AnonymousClassWithInterfaceDemo.java
@@ -0,0 +1,15 @@
+interface AnonymousInterface{
+    void show();
+}
+
+public class AnonymousClassWithInterfaceDemo {
+    public static void main(String[] args) {
+        AnonymousInterface anonymousInterface = new AnonymousInterface() {
+            @Override
+            public void show() {
+                System.out.println("Using show method defined in Interface without creating Object.");
+            }
+        };
+        anonymousInterface.show();
+    }
+}
