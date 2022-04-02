3158c8fdfc589c86230712599c5cb0105613a000
rupesss-09 <rupesss09@gmail.com>
2022-04-02 12:46:43 -0400

    Default Method in Interface

---
+++ src/InterfaceDefaultMethod.java
@@ -0,0 +1,29 @@
+/*
+Now, with default method added there is more than one method, but it will still considered functional interface because there is only one abstract method.
+ */
+@FunctionalInterface
+interface Default{
+    void show();
+    default void displayName(String name){
+        System.out.println("Given name is: " + name);
+    };
+}
+
+class DefaultImp implements Default{
+    public void show(){
+        System.out.println("Defining method declared in interface.");
+    }
+    //We can override the default method as well.
+    @Override
+    public void displayName(String name) {
+        Default.super.displayName(name);
+    }
+}
+
+public class InterfaceDefaultMethod {
+    public static void main(String[] args) {
+        DefaultImp defaultImp = new DefaultImp();
+        defaultImp.show();
+        defaultImp.displayName("Default Method");
+    }
+}
