b4e7c3cb561a8f1ee9a274a295a61f241ae7aa55
rupesss-09 <rupesss09@gmail.com>
2022-04-03 17:33:42 -0400

    Threads Naming and Setting Priority

--- src/com/after5hours/MultiThreadingRunnableInterface.java
+++ src/com/after5hours/MultiThreadingRunnableInterface.java
@@ -72,13 +72,40 @@ public class MultiThreadingRunnableInterface {
                 System.out.println("Hi");
                 try{Thread.sleep(500);}catch(Exception e){};
             }
-        });
+        }, "Hi Thread");
         Thread t2 = new Thread(() -> { //Since, this method have more than 1 line we need the curly brackets as well
             for(int i =0; i <5; i++){
                 System.out.println("Hello");
                 try{Thread.sleep(500);}catch(Exception e){};
             }
-        });
+        }, "Hello Thread");
+
+        /*//Let's print the default name of Threads
+        System.out.println("t1 Thread name is: " + t1.getName());
+        System.out.println("t2 Thread name is: " + t2.getName());*/
+
+        /*//Let's set the name of Threads
+        t1.setName("Hi Thread");
+        t2.setName("Hello Thread");*/
+
+        //Let's print the default name of Threads
+        System.out.println("t1 Thread name is: " + t1.getName());
+        System.out.println("t2 Thread name is: " + t2.getName());
+
+        //We can give priority to Threads with number or CONSTANT
+        System.out.println("Priority to Thread t1: " + t1.getPriority());
+        System.out.println("Priority to Thread t2: " + t2.getPriority());
+
+        //Changing priority to Threads
+        t1.setPriority(10);
+        t2.setPriority(1);
+
+        /*t1.setPriority(Thread.MAX_PRIORITY);
+        t1.setPriority(Thread.MIN_PRIORITY);*/
+
+        //We can give priority to Threads with number or CONSTANT
+        System.out.println("Priority to Thread t1: " + t1.getPriority());
+        System.out.println("Priority to Thread t2: " + t2.getPriority());
 
         t1.start();
         try{Thread.sleep(10);}catch(Exception e){};
