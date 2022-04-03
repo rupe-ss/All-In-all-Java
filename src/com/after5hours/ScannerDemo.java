c6f4c75679d64ee7ad39962d38d874cd7c8d09d2
rupesss-09 <rupesss09@gmail.com>
2022-04-03 13:28:41 -0400

    Existing method inside Scanner class.

--- src/com/after5hours/ScannerDemo.java
+++ src/com/after5hours/ScannerDemo.java
@@ -6,6 +6,7 @@ public class ScannerDemo {
     public static void main(String[] args) {
         System.out.print("Enter a number: ");
         Scanner sc = new Scanner(System.in);
+        //Check out more meth0d inside Scanner class to more about methods like nextInt(). There are many methods that handles the input.
         int n = sc.nextInt();
         System.out.println("Entered number is: " + n);
     }
