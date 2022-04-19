fc27469f76490e8e7272ceca13ac1af244e64a68
rupesss-09 <rupesss09@gmail.com>
2022-04-18 21:57:09 -0400

    forEach method and Consumer interface, forEach method takes Consumer Object as an argument.

---
+++ src/java8newfeatures/ForEachDemo.java
@@ -0,0 +1,30 @@
+package java8newfeatures;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
+/*
+We know lamda expression now, so forEach is Lamda Expression too.
+ */
+public class ForEachDemo {
+    public static void main(String[] args) {
+        ArrayList<Integer> arrayList = new ArrayList<>(Arrays.asList(1,2,3,4,5));
+        ArrayList<Integer> arrayList1 = new ArrayList<>(Arrays.asList(new Integer[]{1,2,3,4,5}));
+        // ArrayList<Integer> arrayList2 = new ArrayList<>(Arrays.asList({1,2,3,4,5}); //We can't do something like this because we need to send object of an array or bunch of numbers.
+        /*
+        Let's talk about how forEach Lamda Expression works. To use Lamda Expression we need Functional Interface, and Consumer is an Functional Interface.
+        We, can see we have to pass Object of Consumer in forEach() method.
+         */
+
+        /*arrayList.forEach(new Consumer<Integer>() {
+            @Override
+            public void accept(Integer integer) {
+                System.out.println(integer);
+            }
+        });*/
+
+        arrayList.forEach(integer -> System.out.println(integer));
+    }
+}
