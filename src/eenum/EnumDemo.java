cbce8671d533f9cd5f238e20c108737f276270e4
rupesss-09 <rupesss09@gmail.com>
2022-04-18 20:58:49 -0400

    Concept behind the enum.

---
+++ src/eenum/EnumDemo.java
@@ -0,0 +1,42 @@
+package eenum;
+/*
+What is happening behind the scene ?
+class Mobile
+{
+    static final Mobile APPLE = new Mobile(100);
+    static final Mobile SAMSUNG = new Mobile();
+    static final Mobile HTC = new Mobile();
+}
+ */
+enum Mobile{
+    APPLE(100), SAMSUNG, NOKIA, ONEPLUS, HTC;
+
+    int price;
+
+    Mobile(){
+        int price = 150;
+        System.out.println("Constructor");
+    }
+
+    Mobile(int p){
+        price = p;
+        System.out.println("Constructor with param.");
+    }
+}
+public class EnumDemo {
+    public static void main(String[] args) {
+        //Everytime Mobile class is called all the Object is defined.
+        Mobile mobile = Mobile.APPLE;
+        System.out.println(mobile);
+
+        Mobile[] mobiles = Mobile.values();
+        for (Mobile mob : mobiles){
+            System.out.println(mob);
+        }
+
+        switch (mobile){
+            case APPLE:
+                System.out.println("Apple Enum");
+        }
+    }
+}
