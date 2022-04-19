126bbd8f97c75db272679400848f0c51b8e6f153
rupesss-09 <rupesss09@gmail.com>
2022-04-19 12:15:32 -0400

    stream, map and more details

--- src/java8newfeatures/streamapi/DoubleValue.java
+++ src/java8newfeatures/streamapi/DoubleValue.java
@@ -2,6 +2,7 @@ package java8newfeatures.streamapi;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.BinaryOperator;
 import java.util.function.Function;
 import java.util.stream.Stream;
 
@@ -30,18 +31,38 @@ public class DoubleValue {
         Stream return Object of stream
          */
 
-        //Below line will give you the Object of stream.
-        Function<Integer, Integer> f = new Function<Integer, Integer>() {
+        /*
+        Below line will give you the Object of stream.
+        Let's reduce below anonymous class to Lamda Expression
+         */
+        /*Function<Integer, Integer> f = new Function<Integer, Integer>() {
             @Override
             public Integer apply(Integer integer) {
                 return integer * 2;
             }
-        };
+        };*/
+        Function<Integer, Integer> f = i -> i * 2;
 
-        Stream<Integer> stream = list.stream();
-        Stream<Integer> s1 = stream.map(f);
-        s1.forEach(System.out::println);
+        /*Stream<Integer> stream = list.stream();
+        Stream<Integer> s1 = stream.map(f);*/
+        //s1.forEach(System.out::println);
 
+        /*
+        All the below code works fine but let's remove some boiler plate code.
+         */
+        /*BinaryOperator<Integer> binaryOperator = new BinaryOperator<Integer>() {
+            @Override
+            public Integer apply(Integer integer, Integer integer2) {
+                return integer + integer2;
+            }
+        };*/
+        //BinaryOperator<Integer> binaryOperator = (c,e) -> c + e;
+        BinaryOperator<Integer> binaryOperator = Integer::sum;
+
+        /*Integer sum = s1.reduce(0,binaryOperator);
+        System.out.println(sum);*/
+
+        System.out.println(list.stream().map(i -> i * 2).reduce(0,Integer::sum));
     }
 
     public static void doubleIt(int i){
