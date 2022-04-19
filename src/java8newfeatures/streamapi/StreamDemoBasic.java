657fa7df7a600e65611660ec37008992bdcb2cb2
rupesss-09 <rupesss09@gmail.com>
2022-04-19 01:24:54 -0400

    Some comment change to make things clear.

--- src/java8newfeatures/streamapi/StreamDemoBasic.java
+++ src/java8newfeatures/streamapi/StreamDemoBasic.java
@@ -32,12 +32,13 @@ public class StreamDemoBasic {
         2.it has so many other feature like filter and more methods to process the data.
         3. Another advantage is that it has two types of method, one is intermediate methods and another is terminate methods.
             Stream are like stream of water, once you use stream you can't use stream again.
-            Below is the example of intermediate method also called lazy evaluation.
+
         */
         System.out.println();
         arrayList.parallelStream().forEach(i -> System.out.print(i + " "));
 
         /*
+        Below is the example of intermediate method also called lazy evaluation.
         Below method won't print hi 100 times because there is no use of i for it won't do anything.
         But we have findFirst() method that will print "hi" one time. findFirst() is optional as well because if you filter all the odd numbers but list didn't have odd number from the beginning. So, this is the reason findFirst() is optional but there is more .orElse() method we can implement after findFirst() if there is no element.
          */
