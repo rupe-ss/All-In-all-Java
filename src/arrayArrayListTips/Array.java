efd29c15d91540b58347a8e4dffe1fb5138672de
rupesss-09 <rupesss09@gmail.com>
2022-04-18 21:38:32 -0400

    Array and ArrayList conversion and tips.

---
+++ src/arrayArrayListTips/Array.java
@@ -0,0 +1,39 @@
+package arrayArrayListTips;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+
+public class Array {
+    public static void main(String[] args) {
+        /*
+        Below is the 3 different ways of defining arrays.
+         */
+        Integer[] iArray = new Integer[5];
+        String[] sArray = {"a", "b", "c"};
+        Integer[] array = new Integer[]{1,2,3,5};
+
+        /*
+        Printing array, see the difference below
+         */
+        int[] intArray = {1,2,3,4,5,6};
+        String str = Arrays.toString(intArray);
+        System.out.println(intArray);
+        System.out.println(str);
+
+        /*
+        Creating an ArrayList from an array
+         */
+        String[] strings = {"a", "b", "c", "d", "e"};
+        String[] strings1 = {"a", "b", "c", "d", "e", "a"};
+        ArrayList<String> arrayList = new ArrayList<>(Arrays.asList(strings));
+        HashSet<String> hashSet = new HashSet<>(Arrays.asList(strings1));
+        arrayList.forEach(i -> System.out.print(i + " "));
+        System.out.println();
+        hashSet.forEach(i -> System.out.print(i + " "));
+        /*
+        Check if arrays contains a certain values.
+         */
+        System.out.println("\n" + Arrays.asList(strings).contains("d"));
+    }
+}
