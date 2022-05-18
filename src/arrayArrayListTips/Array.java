e630468f51b59dbb1ae3d8621b80548d64ec31ef
rupesss-09 <rupesss09@gmail.com>
2022-05-17 21:43:36 -0400

    Updating and learning more about arrays and arrayList.

--- src/arrayArrayListTips/Array.java
+++ src/arrayArrayListTips/Array.java
@@ -7,7 +7,7 @@ import java.util.HashSet;
 public class Array {
     public static void main(String[] args) {
         /*
-        Below is the 3 different ways of defining arrays.
+        Below is the 4 different ways of defining arrays.
          */
         Integer[] iArray = new Integer[5];
         String[] sArray = {"a", "b", "c"};
@@ -19,15 +19,23 @@ public class Array {
          */
         int[] intArray = {1,2,3,4,5,6};
         String str = Arrays.toString(intArray);
-        System.out.println(intArray);
-        System.out.println(str);
+        System.out.println(intArray); //This will print a hash value bcoz intArray is an Object.
+        System.out.println(intArray.getClass().getSimpleName()); //This will print int[]
+        System.out.println(str); //This will print [1, 2, 3, 4, 5, 6]
+        System.out.println(str.getClass().getName()); //This will print java.lang.String
+        System.out.println("Length of String 'str' = " + str.length()); //This will print 18, meaning this entire is string "[1, 2, 3, 4, 5, 6]".
 
         /*
         Creating an ArrayList from an array
          */
+        System.out.println("***********-------------************");
         String[] strings = {"a", "b", "c", "d", "e"};
         String[] strings1 = {"a", "b", "c", "d", "e", "a"};
-        ArrayList<String> arrayList = new ArrayList<>(Arrays.asList(strings));
+        ArrayList<String> arrayList = new ArrayList<>(Arrays.asList(strings)); //This will change array to arrayList
+        System.out.println(strings.getClass().getName()); //This will print [Ljava.lang.String; not sure what this is lol
+        System.out.println(strings1.getClass().getSimpleName()); //This will print String[] , meaning array of string
+
+        //Collection Set doesn't have a duplicate values.
         HashSet<String> hashSet = new HashSet<>(Arrays.asList(strings1));
         arrayList.forEach(i -> System.out.print(i + " "));
         System.out.println();
@@ -36,5 +44,7 @@ public class Array {
         Check if arrays contains a certain values.
          */
         System.out.println("\n" + Arrays.asList(strings).contains("d"));
+        //In collection you can check if List contains a certain value
+        System.out.println(hashSet.contains("c"));
     }
 }
