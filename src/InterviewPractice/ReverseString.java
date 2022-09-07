b537dd38124c23b8a88764b3d897fe63dd589de8
rupesss-09 <rupesss09@gmail.com>
2022-09-06 22:36:18 -0400

    Reversed string with StringBuilder() and without StringBuilder()

---
+++ src/InterviewPractice/ReverseString.java
@@ -0,0 +1,24 @@
+package InterviewPractice;
+
+public class ReverseString {
+
+    private static String reverseStringUsingStringBuilder(String string){
+        //StringBuilder makes string mutable, we know string is immutable.
+        return new StringBuilder(string).reverse().toString();
+    }
+
+    //Try to reverse string without using StringBuilder()
+    private static String reverseStringWithoutUsingStringBuilder(String string){
+        String str = "";
+        for(int i = 0; i < string.length(); i++){
+            char ch = string.charAt(i);
+            str = ch + str;
+        }
+        return str;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(reverseStringUsingStringBuilder("Reverse Me"));
+        System.out.println(reverseStringWithoutUsingStringBuilder("Without"));
+    }
+}
