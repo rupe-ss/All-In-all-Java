950629fbfae0a025e78ee1290d1e509f6f8f3903
rupesss-09 <rupesss09@gmail.com>
2022-09-07 14:41:23 -0400

    Simple inbuilt method to convert integer into binary number and more practice with sorting array.

---
+++ src/InterviewPractice/IntegerToBinary.java
@@ -0,0 +1,26 @@
+package InterviewPractice;
+
+import java.util.Arrays;
+
+public class IntegerToBinary {
+
+    private static int[] sortArray(int[] array){
+        int temp;
+        for(int i = 0; i < array.length; i++){
+            for(int j = i + 1; j < array.length; j++){
+                if(array[i] > array[j]){
+                    temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                }
+            }
+        }
+        return array;
+    }
+    public static void main(String[] args) {
+        int num = 567;
+        System.out.println(Integer.toBinaryString(num));
+        int[] numbers = {2,5,7,8,12,1,2};
+        System.out.println(Arrays.toString(sortArray(numbers)));
+    }
+}
