2c1e522e6d066c068935b9c422c7ff452fb12156
rupesss-09 <rupesss09@gmail.com>
2022-09-07 14:54:34 -0400

    Checking if number is odd or even with Quotient operator and bitwise(And Operator).

---
+++ src/InterviewPractice/CheckIfNumberIsOddOrEven.java
@@ -0,0 +1,22 @@
+package InterviewPractice;
+
+public class CheckIfNumberIsOddOrEven {
+    private static String check(int number){
+        if((number & 1) == 0){
+            return "Even";
+        }
+        return "Odd";
+    }
+
+    private static String checkNumber(int number){
+        if((number % 2) == 0){
+            return "Even";
+        }
+        return "Odd";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(check(0));
+        System.out.println(checkNumber(17));
+    }
+}
