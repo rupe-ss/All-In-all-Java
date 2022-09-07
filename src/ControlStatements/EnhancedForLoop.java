dc933995c099cdfda711b4b1d580c9444349bb8c
rupesss-09 <rupesss09@gmail.com>
2022-09-07 14:42:05 -0400

    Using enhanced for loop to print the numbers in an array.

---
+++ src/ControlStatements/EnhancedForLoop.java
@@ -0,0 +1,11 @@
+package ControlStatements;
+
+public class EnhancedForLoop {
+    public static void main(String[] args) {
+        int[] array = {2,3,4,5,6,7};
+
+        for(int i: array){
+            System.out.println(i);
+        }
+    }
+}
