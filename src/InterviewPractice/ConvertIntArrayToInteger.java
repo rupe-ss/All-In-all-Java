04b17011841f4bd564a7fe4c984c3edc50dafd56
rupesss-09 <rupesss09@gmail.com>
2022-09-08 10:26:06 -0400

    Converting int[] into Integer[] using stream api.

--- src/InterviewPractice/ConvertIntArrayToInteger.java
+++ src/InterviewPractice/ConvertIntArrayToInteger.java
@@ -1,5 +1,7 @@
 package InterviewPractice;
 
+import java.util.Arrays;
+
 public class ConvertIntArrayToInteger {
     private static int convert(int[] array){
         StringBuilder string = new StringBuilder();
@@ -10,8 +12,14 @@ public class ConvertIntArrayToInteger {
         return number;
     }
 
+    //Converting int[] array to Integer[] array using stream api
+    private static Integer[] convertInteger(int[] array){
+        return Arrays.stream(array).boxed().toArray(Integer[]::new);
+    }
+
     public static void main(String[] args) {
         int[] numbers = {2,3,4};
         System.out.println(convert(numbers));
+        System.out.println(Arrays.toString(convertInteger(numbers)) + "\n" + convertInteger(numbers).getClass().getSimpleName());
     }
 }
