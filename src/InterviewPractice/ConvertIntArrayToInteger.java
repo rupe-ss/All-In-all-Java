dcb3543067ba32fbfd5b2fae174391141ea9d8c4
rupesss-09 <rupesss09@gmail.com>
2022-09-09 00:34:12 -0400

    Converting array of Integer[] to int[] array. We can do this by streaming Integer to int.

--- src/InterviewPractice/ConvertIntArrayToInteger.java
+++ src/InterviewPractice/ConvertIntArrayToInteger.java
@@ -1,6 +1,7 @@
 package InterviewPractice;
 
 import java.util.Arrays;
+import java.util.stream.Stream;
 
 public class ConvertIntArrayToInteger {
     private static int convert(int[] array){
@@ -14,12 +15,21 @@ public class ConvertIntArrayToInteger {
 
     //Converting int[] array to Integer[] array using stream api
     private static Integer[] convertInteger(int[] array){
-        return Arrays.stream(array).boxed().toArray(Integer[]::new);
+        return Arrays.stream(array)     //Stream array
+                .boxed()            //Stream <Integer>
+                .toArray(Integer[]::new);   //This will make a new array
+    }
+
+    private static int[] convertIntegerArrayToInt(Integer[] nums){
+        int[] result = Stream.of(nums).mapToInt(x -> x).toArray();
+        return result;
     }
 
     public static void main(String[] args) {
         int[] numbers = {2,3,4};
+        Integer[] integerNums = {2,3,4,5,6};
         System.out.println(convert(numbers));
-        System.out.println(Arrays.toString(convertInteger(numbers)) + "\n" + convertInteger(numbers).getClass().getSimpleName());
+        System.out.println(Arrays.toString(convertInteger(numbers)) + "   -   " + convertInteger(numbers).getClass().getSimpleName());
+        System.out.println(Arrays.toString(convertIntegerArrayToInt(integerNums)) + "   -   " + convertIntegerArrayToInt(integerNums).getClass().getSimpleName());
     }
 }
