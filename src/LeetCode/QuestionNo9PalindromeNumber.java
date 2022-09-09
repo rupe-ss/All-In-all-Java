fb71c9f61fc4d7cee5a04e7d1606dbb66fd9e834
rupesss-09 <rupesss09@gmail.com>
2022-09-08 21:33:50 -0400

    Different way to find if integer is palindrome or not. Got idea from leetcode.

--- src/LeetCode/QuestionNo9PalindromeNumber.java
+++ src/LeetCode/QuestionNo9PalindromeNumber.java
@@ -1,6 +1,8 @@
 package LeetCode;
 
 public class QuestionNo9PalindromeNumber {
+
+    //This solution is done by me personally.
     public static boolean isPalindrome(int x) {
         if(x < 0){
             return false;
@@ -13,7 +15,20 @@ public class QuestionNo9PalindromeNumber {
         return false;
     }
 
+    //Simple and faster way to do it.
+    public static boolean leetCodeDiscussSolution(int x){
+        int rev = 0;
+        for(int i = x; i > 0; i/= 10){
+            rev = rev * 10 + (i%10);
+        }
+        if(rev == x){
+            return true;
+        }
+        return false;
+    }
+
     public static void main(String[] args) {
-        System.out.println(isPalindrome(121));
+        System.out.println(isPalindrome(12121));
+        System.out.println(leetCodeDiscussSolution(12121));
     }
 }
