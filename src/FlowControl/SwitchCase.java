6c8d0b961d4040b17e239e3838d13362cba6f3a9
rupesss-09 <rupesss09@gmail.com>
2022-09-12 14:01:22 -0400

    Switch Control practice and created Flow-Control folder as well.

---
+++ src/FlowControl/SwitchCase.java
@@ -0,0 +1,18 @@
+package FlowControl;
+
+public class SwitchCase {
+
+    //Practicing switch-case
+    public static void main(String[] args) {
+        char ch = 'A';
+
+        switch (ch){
+            case 'A':
+                System.out.println("This is first character of English alphabet.");
+                break;
+
+            default:
+                System.out.println("This is a default case in switch.");
+        }
+    }
+}
