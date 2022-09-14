72827f5d0d8f31b766acb6fa323bf911ac7dfbed
rupesss-09 <rupesss09@gmail.com>
2022-09-14 15:40:04 -0400

    Longest common prefix more practice. This time done by myself without any reference.

---
+++ src/InterviewPractice/LongestPrefix.java
@@ -0,0 +1,32 @@
+package InterviewPractice;
+
+import java.util.Arrays;
+
+public class LongestPrefix {
+
+    private static String longestPrefix(String[] array){
+
+        if(array[0].length() == 0)
+            return "";
+
+        String prefix = "";
+        Arrays.sort(array);
+        String firstElement = array[0];
+
+        for(int i = 0; i < firstElement.length(); i++){
+            prefix = firstElement.substring(0,i + 1);
+            for(int j = 1; j < array.length; j++){
+                if(array[j].substring(0,i+1).equals(prefix))
+                    continue;
+                prefix = firstElement.substring(0, i);
+                return prefix.substring(0,i);
+            }
+        }
+        return prefix;
+    }
+
+    public static void main(String[] args) {
+        String[] array = {"Flower", "Flour", "Florine"};
+        System.out.println(longestPrefix(array));
+    }
+}
