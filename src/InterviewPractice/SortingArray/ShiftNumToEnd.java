6c80e6473d47d385eff36ffbf68974993eb8e648
rupesss-09 <rupesss09@gmail.com>
2022-09-21 23:07:42 -0400

    In an array of an integer shift a given integer to the end of an array.

---
+++ src/InterviewPractice/SortingArray/ShiftNumToEnd.java
@@ -0,0 +1,29 @@
+package InterviewPractice.SortingArray;
+
+import java.util.Arrays;
+
+public class ShiftNumToEnd {
+
+    /*
+    An array is provided with integer, you need to shift that integer to an end.
+    int[]arr = {3,4,5,0,0,2,7,0}
+     */
+    private static int[] shift(int[] arr, int num){
+        int j = arr.length - 1;
+
+        for(int i = j; i >=0; i--){
+            if(arr[i] == num){
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                j--;
+            }
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        int[] array = {3,4,2,5,0,2,2,7,0};
+        System.out.println(Arrays.toString(shift(array, 2)));
+    }
+}
