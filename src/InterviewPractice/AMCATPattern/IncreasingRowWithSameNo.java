b88383dd50fcc7820d502a9755996431de2038ab
rupesss-09 <rupesss09@gmail.com>
2022-09-26 13:04:20 -0400

    Interview question pattern, print same number in each row multiple times.

---
+++ src/InterviewPractice/AMCATPattern/IncreasingRowWithSameNo.java
@@ -0,0 +1,22 @@
+package InterviewPractice.AMCATPattern;
+
+import java.util.Scanner;
+
+public class IncreasingRowWithSameNo {
+
+    //This code will print the number of row inserted but with same number no changing.
+    private static void print(int noRow){
+        for(int i = 1; i <= noRow; i++){
+            for(int j = 1; j <= i; j++){
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.print("Enter no of row: ");
+        Scanner scanner = new Scanner(System.in);
+        print(scanner.nextInt());
+    }
+}
