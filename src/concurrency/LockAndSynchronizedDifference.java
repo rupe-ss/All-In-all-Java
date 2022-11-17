9734aa64a2554d89e8b03db7f83f2a4741e7df2d
Rupesh Khadka <rupesss09@gmail.com>
2022-11-17 13:24:44 -0500

    Difference between lock and synchronized block and how to use lock

---
+++ src/concurrency/LockAndSynchronizedDifference.java
@@ -0,0 +1,44 @@
+package concurrency;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+
+public class LockAndSynchronizedDifference {
+
+    public class CounterSynchronized {
+        private long count = 0;
+
+        public synchronized void inc() {
+            this.count++;
+        }
+
+        public synchronized long getCount() {
+            return this.count;
+        }
+    }
+
+    public class CounterLock {
+        private long count = 0;
+
+        private Lock lock = new ReentrantLock();
+
+        public void inc() {
+            try {
+                lock.lock();
+                this.count++;
+            } finally {
+                lock.unlock();
+            }
+        }
+
+        public long getCount() {
+            try {
+                lock.lock();
+                return this.count;
+            } finally {
+                lock.unlock();
+            }
+        }
+    }
+}
