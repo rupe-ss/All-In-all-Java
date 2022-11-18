4e956ac28c6169a643124d16d06347c02e7ec264
Rupesh Khadka <rupesss09@gmail.com>
2022-11-18 17:37:22 -0500

    Some update after understanding real difference between Class Level Lock and Object Level Lock.

--- src/concurrency/synchronizedexchanger/SynchronizedObjectLevelLocking.java
+++ src/concurrency/synchronizedexchanger/SynchronizedObjectLevelLocking.java
@@ -1,20 +1,19 @@
 package concurrency.synchronizedexchanger;
 
-public class SynchronizedObjectLevelLocking {
-
-    private static int counter1 = 0;
-    private static int counter2 = 0;
+class SynchronizedOnject {
+    private int counter1 = 0;
+    private int counter2 = 0;
 
     //Here problem with synchronized keyword, even though increment1() and increment2() methods are independent to each other thread1 and thread2 can't access both method at same time because synchronized will lock the instance of SynchronizedObjectLevelLocking.class or Object of SynchronizedObjectLevelLocking.class
-    private static synchronized void increment1(){
+    private synchronized void increment1(){
         counter1++;
     }
 
-    private static synchronized void increment2(){
+    private synchronized void increment2(){
         counter2++;
     }
 
-    private static void process(){
+    public void process(){
         Thread thread1 = new Thread(()-> {
             for (int i = 0; i < 1000; i++){
                 increment1();
@@ -32,8 +31,12 @@ public class SynchronizedObjectLevelLocking {
         thread1.start();
         thread2.start();
     }
+}
+
+public class SynchronizedObjectLevelLocking {
 
     public static void main(String[] args) {
-        process();
+        SynchronizedOnject synchronizedOnject = new SynchronizedOnject();
+        synchronizedOnject.process();
     }
 }
