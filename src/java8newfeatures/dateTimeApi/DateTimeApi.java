bfa697857ab84f3dbe1982ccbb027f542bc5e4b1
Rupesh Khadka <rupesss09@gmail.com>
2023-08-11 14:02:42 -0400

    Java 1.8 DateTimeApi, review.

--- src/java8newfeatures/dateTimeApi/DateTimeApi.java
+++ src/java8newfeatures/dateTimeApi/DateTimeApi.java
@@ -1,7 +1,13 @@
-package java8newfeatures;
+package java8newfeatures.dateTimeApi;
 
-import java.text.*;
-import java.time.*;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.Month;
+import java.time.ZoneId;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class DateTimeApi {
     /*
@@ -9,6 +15,7 @@ public class DateTimeApi {
     1. Earlier version of Java first issue with Data Time Api is there were 2 packages java.util and java.sql with same class.
     2. Second issue was, we need to import java.text.* packages while working with date formatting.
     3. Third issue was Date Object wasn't thread safe. If multiple thread was working with Date object then there can be a problem.
+
     New Version Java 8
     1. Date Time is immutable. It will create a new object for you everytime you try to change it.
     2. We use different package(java.time.LocalDate) now so there is no confusion.
@@ -16,6 +23,8 @@ public class DateTimeApi {
     public static void main(String[] args) {
         LocalDate localDate = LocalDate.now();
         System.out.println(localDate); //This will print only the date.
+
+        //Month is a ENUM not a class
         LocalDate localDate1 = LocalDate.of(1989, Month.APRIL,12);
         System.out.println(localDate1);
         /*LocalDate localDate2 = LocalDate.of(1989, Month.FEBRUARY,29); //This will throw an Exception now.
@@ -27,9 +36,11 @@ public class DateTimeApi {
         LocalDateTime localDateTime = LocalDateTime.now();
         System.out.println(localDateTime);
 
-        /*//Get all the time Zone
+        //Get all the time Zone
         //Learn how to filter this set
-        ZoneId.getAvailableZoneIds().forEach(i-> System.out.println(i));*/
+        //ZoneId.getAvailableZoneIds().forEach(i-> System.out.println(i));
+        Set<String> americanZones = ZoneId.getAvailableZoneIds().stream().filter(i -> i.contains("America")).collect(Collectors.toSet());
+        System.out.println(americanZones);
 
         //Let's look some machine readable object
         Instant instant = Instant.now();
