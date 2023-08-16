196f42e677d217e8a259f882cb8a41bd27c0eda3
Rupesh Khadka <rupesss09@gmail.com>
2023-08-15 23:52:00 -0400

    Tree Node - Checking if tree node is same or not, first storing add Node values in List using InOrder Transversal.

---
+++ src/LeetCode/Question100SameTree/Question100First.java
@@ -0,0 +1,27 @@
+package LeetCode.Question100SameTree;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.BiPredicate;
+
+public class Question100First {
+    public static void main(String[] args) {
+        List<Integer> pList = Arrays.asList(1,2,3);
+        List<Integer> qList = Arrays.asList(1,2,3);
+
+        System.out.println(pList == qList);
+
+        System.out.println(biPredicate.test(pList, qList));
+
+
+    }
+
+    private static BiPredicate<List<Integer>, List<Integer>> biPredicate = (p, q) -> {
+        boolean same = false;
+        for(int i = 0; i<p.size(); i++){
+            if(p.get(i) == q.get(i)) same = true;
+            else same = false;
+        }
+        return same;
+    };
+}
